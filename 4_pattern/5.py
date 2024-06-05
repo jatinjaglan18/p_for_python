0c07c5c3bf07819e1a702c8863419f149d36e408
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-05 18:53:55 +0530

    Star pattern

---
+++ 4_pattern/5.py
@@ -0,0 +1,33 @@
+'''
+Code : Star Pattern
+
+Print the following pattern
+Pattern for N = 4
+...*
+..*** 
+.*****
+*******
+ 
+The dots represent spaces.
+
+Input Format :
+N (Total no. of rows)
+Output Format :
+Pattern in N lines
+Constraints :
+0 <= N <= 50
+
+'''
+
+n = int(input())
+
+for row in range(1,n+1):
+    for space in range(n-row):
+        print(' ', end='')
+    
+    for star in range(n-row,n):
+        print('*', end ='')
+    
+    for s in range(2*row-2,row-1,-1):
+        print('*', end='')
+    print()
