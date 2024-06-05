3cea5093cf7b42c2669c5f92466f408153067349
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-05 19:39:22 +0530

    Star pattern

--- 4_pattern/5.py
+++ 4_pattern/5.py
@@ -25,9 +25,9 @@ for row in range(1,n+1):
     for space in range(n-row):
         print(' ', end='')
     
-    for star in range(n-row,n):
+    for star in range(2*row,1,-1):
         print('*', end ='')
     
-    for s in range(2*row-2,row-1,-1):
-        print('*', end='')
+    '''for s in range(2*row-2,row-1,-1):
+        print('*', end='')'''
     print()
