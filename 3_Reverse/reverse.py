cd75ca1999fa27d95ce117814819ba4f6def83d6
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-05 15:08:45 +0530

    Palindrome

--- 3_Reverse/reverse.py
+++ 3_Reverse/reverse.py
@@ -1,11 +1,20 @@
 i = int(input())
 
-rev_num=0
-while i>0:
-    num = i % 10
+def revers(i):
 
-    rev_num = (rev_num * 10) + num
+    rev_num=0
+    while i>0:
+        num = i % 10
 
-    i = i // 10
+        rev_num = (rev_num * 10) + num
+
+        i = i // 10
+
+    return rev_num
+
+rev_num = revers(i)
 
 print(rev_num)
+
+#check palindrome
+print(int(rev_num)==i)
