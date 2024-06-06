6cb9b817977ec04b1209da9549a07fd8da2bc1e7
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-06 15:00:08 +0530

    is prime

--- 5_prime/1_isprime.py
+++ 5_prime/1_isprime.py
@@ -3,7 +3,7 @@ t = int(input("How many numbers to check: "))
 for i in range(t):
     j = int(input("Enter the number to check: "))
 
-    for a in range(2,j):
+    for a in range(2,int(j**0.5)):       # ** repersent as exponential ; it is used find out Square root
         if j % a == 0:
             print(j,'is not a prime number')
             break
