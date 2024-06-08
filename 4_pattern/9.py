9f4cd604f5862614b20c6b11a06e0108f9e1f780
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-08 21:58:51 +0530

    Number patterns

---
+++ 4_pattern/9.py
@@ -0,0 +1,49 @@
+n = int(input())
+
+'''
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+'''
+k = 1
+for i in range(n):
+    for j in range(i+1):
+        print(k, end=' ')
+        k+=1
+    print()
+
+'''
+0 
+1 1 
+2 3 5 
+8 13 21 34 
+55 89 144 233 377
+'''
+a = 0
+b = 1
+for i in range(n):
+    for j in range(i+1):
+        
+        print(a, end=' ')
+        c = a+b
+        a = b
+        b = c
+    print()
+
+'''
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+'''
+for i in range(n):
+    val = 1
+    for j in range(i+1):   
+        print(val, end=' ')
+        val = (val * (i-j))//(j+1)  #use concepts of combination 
+
+    print()
+
