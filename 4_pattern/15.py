253f61f52eb04575f1cd205a8ffb32c812e3bfa2
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-09 20:57:29 +0530

    W

---
+++ 4_pattern/15.py
@@ -0,0 +1,20 @@
+'''
+n = 5
+*   *
+*   *
+* * *
+** **
+*   *
+'''
+
+n = int(input())
+for i in range(1,n+1):
+    for j in range(1, n + 1):
+        if j == 1 or j == n:
+            print('*', end = '')
+        elif i > n // 2 and (i == j or i + j == n + 1):
+                print('*', end ='')
+        else:
+            print(' ', end='')
+            
+    print()
