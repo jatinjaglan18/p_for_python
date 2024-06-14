8224a510b7aa72a6b602f22582b53d1cdd877e4b
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-14 12:03:52 +0530

    Sum of two numbers given as an array

--- array/4.py
+++ array/4.py
@@ -1,4 +1,4 @@
-#Sum of two array
+#Sum of two numbers given as an array
 
 from array import *
 n1 = int(input())
@@ -13,6 +13,8 @@ if len(arr2) > len(arr1):
     arr = arr2
     arr2 = arr1
 
+'''
+#Element wise sum of two arrays
 i = 0
 j = 0
 
@@ -23,4 +25,36 @@ while i < len(arr) or j < len(arr2):
     else:
         print(arr[i] + arr2[j])
         i += 1
-        j += 1
+        j += 1'''
+
+sum_arr = array('i',[0 for i in range(len(arr))])       #array('i')
+
+i = len(arr) - 1
+j = len(arr2) - 1
+k = len(sum_arr) -1
+
+c = 0
+
+while k >= 0:
+    d = c
+    if i >= 0 :
+        d += arr[i]
+    if j >= 0:
+        d += arr2[j]
+    
+    c = d // 10
+    d = d % 10
+
+    sum_arr[k] = d                           #array.append(d)
+
+    i -= 1
+    j -= 1
+    k -= 1
+    
+
+if c != 0:
+    print(c)
+
+for i in range(len(sum_arr)):                #(len(sum_arr)-1,-1,-1)
+    print(sum_arr[i])
+
