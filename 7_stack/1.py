b6e9afc65c8443b1e8c782642b633beb1b064342
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-20 20:48:52 +0530

    Stack using OOPS

--- 7_stack/1.py
+++ 7_stack/1.py
@@ -38,3 +38,55 @@ stack.pop()
 print(stack, 'Size',len(stack), 'Top Element', stack[len(stack)-1])
 
 stack.pop()
+
+#Stack Using OOPS
+class Stack:
+    def __init__(self,size):
+        self.arr = []
+        self.size = size
+
+    def is_empty(self):
+        return len(self.arr) == 0
+    def get_size(self):    
+        return len(self.arr)
+    
+    def see(self):
+        for i in self.arr:
+            print(self.arr[-i], end = ' ')
+        print()
+    
+    def peek(self):
+        if self.is_empty():
+            print( 'Stack is Empty' ) 
+        else:
+            print( self.arr[-1])
+
+    def push(self,val):
+        if self.get_size() == self.size:
+            print('Stack Overflow')
+        else:
+            self.arr.append(val)
+
+    def pop(self):
+        if self.is_empty():
+            print('Stack Underflow')
+        else:
+            print(self.arr.pop())
+        
+
+
+
+stack = Stack(5)
+print(stack.size)
+stack.push(1)
+stack.peek()
+stack.push(2)
+stack.peek()
+stack.push(3)
+stack.see()
+stack.peek()
+stack.pop()
+stack.peek()
+stack.pop()
+stack.peek()
+stack.pop()
