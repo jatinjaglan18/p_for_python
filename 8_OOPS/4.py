aea1655db633ca3e974fa10adca63c3a749568fb
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-22 22:38:35 +0530

    Linked List Ques

--- 8_OOPS/4.py
+++ 8_OOPS/4.py
@@ -70,7 +70,7 @@ class LinkedList:
             temp = self.head
             for i in range(i):
                 temp = temp.next
-            print(temp.data)
+            return temp.data
 
     def addFirst(self,val):
         if self.size == 0:
@@ -126,15 +126,189 @@ class LinkedList:
                 temp = temp.next
             temp.next = temp.next.next
             self.size -= 1
-
-    def reverse(Self):
-
-        temp = Self.head
-        stack = []
-        while temp != None:
-            stack.append(temp.data)
+    
+    def get_node(self,idx):
+        temp = self.head
+        for i in range(idx):
             temp = temp.next
+        return temp
 
-        while len(stack) != 0:
-            print(stack[-1], end = ' ')
-            stack.pop()
+    #Reverse linkedlist using node.data
+    def reverse(self):
+        for i in range((self.size//2)+1):
+            v1 = self.get_node(i)
+            v2 = self.get_node(self.size - (i+1))
+            
+            v1.data, v2.data = v2.data, v1.data  #Swap
+
+    #Reverse using node.next pointer
+    def reverse1(self):
+        temp = self.tail
+        for i in range(self.size-1, -1, -1):
+            right = self.get_node(i)
+            if i != 0:
+                left = self.get_node(i-1)
+                right.next = left
+            else:
+                right.next = None
+                self.tail = right
+        self.head = temp
+
+    def mid(self):
+        s = self.head
+        f = self.head
+        while f.next != None and f.next.next != None:
+            s = s.next
+            f = f.next.next
+        return s.data
+
+class Stack:
+    def __init__(self):
+        self.s = LinkedList()
+        self.size = 0
+    
+    def get_size(self):
+        return self.size
+    def peek(self):
+        return self.s.tail.data
+    
+    def push(self,val):
+        self.s.addLast(val)
+        self.size += 1
+
+    def pop(self):
+        #print(self.s.tail.data)
+        self.s.removeLast()
+        self.size -=1
+
+
+class Queue:
+    def __init__(self):
+        self.q = LinkedList()
+        self.size = 0
+    def get_size(self):
+        return self.size
+    def peek(self):
+        print(self.q.head.data)
+    def enqueue(self,val):
+        self.q.addLast(val)
+        self.size += 1
+    def dequeue(self):
+        self.q.removeFirst()
+        self.size -= 1
+
+
+def k_from_last(l,k):
+    i = l.head
+    j = l.head
+
+    for x in range(k):
+        j = j.next
+    
+    while j != l.tail:
+        i = i.next
+        j = j.next
+    
+    return i.data
+
+#Using stack
+def k_from_last(l,k):
+    s = Stack()
+    temp = l.head
+    while temp != None:
+        s.push(temp.data)
+        temp = temp.next
+    
+    for i in range(k):
+        s.pop()
+    return s.peek()
+
+
+def MergeSortSort(l1,l2):
+    temp1 = l1.head
+    temp2 = l2.head
+
+    l = LinkedList()
+    while temp1 != None and temp2 != None:
+        if temp1.data <= temp2.data:
+            l.addLast(temp1.data)
+            temp1 = temp1.next
+        elif temp2.data < temp1.data:
+            l.addLast(temp2.data)
+            temp2 = temp2.next
+        
+        
+    while temp1 != None:
+            l.addLast(temp1.data)
+            temp1 = temp1.next
+
+    while temp2 != None:
+            l.addLast(temp2.data)
+            temp2 = temp2.next
+    return l
+
+def midNode(h,t):               #h = head node, t = tail node
+        s = h
+        f = s
+        while f != t and f.next != t:
+            s = s.next
+            f = f.next.next
+        return s
+
+
+def MergeSort(h,t):
+    if h == t :
+        r = LinkedList()
+        r.addLast(h.data)
+        return r
+    
+    mid = midNode(h,t)
+    fsh = MergeSort(h,mid)
+    ssh = MergeSort(mid.next, t)
+    res = LinkedList()
+    res = MergeSortSort(fsh,ssh)
+    return res
+
+
+l = LinkedList()
+l.addLast(1)
+l.addLast(2)
+l.addLast(5)
+l.addLast(6)
+l.addLast(7)
+l.addLast(11)
+l.addLast(13)
+l.addLast(12)
+
+
+l.display()
+
+def remove_duplicates(l):
+    i = l.head
+    j = l.head.next
+
+    while j != None:
+        if i.data == j.data:
+            i.next =  j.next
+            l.size -= 1
+            j = j.next
+        else:
+            i = i.next
+            j = j.next
+
+def odd_even(l):
+    temp = l.head
+    e = LinkedList()
+    o = LinkedList()
+    for i in range(l.size):
+        if temp.data % 2 == 0:
+            e.addLast(temp.data)
+        else:
+            o.addLast(temp.data)
+        temp = temp.next
+    
+    o.tail.next = e.head
+    return o
+
+z = odd_even(l)
+z.display()
