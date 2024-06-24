b35732084a035cc63d776bf0b32a3a34b1dd9047
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-24 18:56:31 +0530

    x to power n

--- 9_recursion/2.py
+++ 9_recursion/2.py
@@ -1,4 +1,4 @@
-#factorial
+#Factorial
 n = int(input())
 
 def fac(n):
@@ -8,3 +8,27 @@ def fac(n):
     res = n*fnm1
     return res
 print(fac(n))
+
+#Linear Power
+def linear_power(x,n):
+    if n == 0:
+        return 1
+    p = linear_power(x,n-1)
+    res = x * p
+    return res
+x = int(input())
+n = int(input())
+print(linear_power(x,n))
+
+def log_power(x,n):
+    if n == 0:
+        return 1
+    if n % 2 == 0:
+        half = log_power(x,n//2)
+        res = half*half
+        return res
+    else:
+        half = log_power(x,n//2)
+        res = half*half*x
+        return res
+print(log_power(x,n))
