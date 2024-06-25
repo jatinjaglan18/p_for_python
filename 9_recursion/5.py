84d570e4452514eedf4e88817f3ff15cd84327bf
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-25 18:47:02 +0530

    array opreations using Recursion

--- 9_recursion/5.py
+++ 9_recursion/5.py
@@ -13,15 +13,14 @@ def display(arr,i):
     print(arr[i])
     display(arr,i+1)
 
-
+#display reverse contents of array using recursion
 def displayr(arr,i):
     if i == len(arr):
         return
     displayr(arr,i+1)
     print(arr[i])
 
-
-
+#Maximum of an array
 def maxi(arr,i):
     if i == len(arr):
         return len(arr)-1
@@ -31,8 +30,7 @@ def maxi(arr,i):
     else:
         return ele
 
-
-
+#Find First ouccurrence of array
 def findFirst(arr,i,val):
     if i == len(arr):
         return -1
@@ -41,7 +39,7 @@ def findFirst(arr,i,val):
     else:
         return findFirst(arr,i+1,val)
         
-
+#Find last ouccurrence of array
 def findLast(arr,i,val):
     if i == len(arr):
         return -1
@@ -54,4 +52,28 @@ def findLast(arr,i,val):
     else: 
         return ouc
 
-print(findLast(arr,0,8))
+
+#Print all indices of a value in an array using insert function
+def allIndices(arr,i,val):
+    if i == len(arr):
+        L = []
+        return L
+
+    L = allIndices(arr,i+1,val)
+    if arr[i] == val:
+        L.insert(0,i)
+    return L
+
+#Print all indices of a value in an array without using insert function
+def allIndices(arr,i,val,count):
+    if i == len(arr):
+        arr1 = [0 for i in range(count)]
+        return arr1
+    
+    if arr[i] == val:
+        arr1 = allIndices(arr, i+1 , val, count+1)
+        arr1[count] = i
+        return arr1
+    else:
+        arr1 = allIndices(arr, i+1, val, count)
+        return arr1
