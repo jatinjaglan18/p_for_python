2f88a58873e113167d2ead7974a7b7fb4d29feef
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-06-28 14:40:41 +0530

    Print Maze Paths

--- 9_recursion/9.py
+++ 9_recursion/9.py
@@ -22,3 +22,32 @@ def mazePaths(sr,sc,dr,dc):
     return res
 
 print(mazePaths(1,1,n,m))
+
+
+#Print paths of maze
+
+def print_maze_paths(sr,sc,dr,dc,path):
+    #Call Smart, Base Case Normal
+    if sr == dr and sc == dc:
+        print(path)
+        return 
+    
+    if sr < dr:         #Jis rashte jana nhi uska pta hi kyu lagana
+        print_maze_paths(sr+1, sc , dr,dc,path + 'h')
+    if sc < dc :
+        print_maze_paths(sr,sc+1 ,dr,dc,path + 'v')
+
+print_maze_paths(1,1,n,m,'')
+
+
+def print_maze_paths(sr,sc,dr,dc,path):
+    #Call Stupid, Base Case Normal
+    if sr > dr or sc > dc:      #rashte ka pta h pr jana nhi h us rashte 
+        return
+    if sr == dr and sc == dc:
+        print(path)
+        return 
+    print_maze_paths(sr+1, sc , dr,dc,path + 'h')
+    print_maze_paths(sr,sc+1 ,dr,dc,path + 'v')
+
+print_maze_paths(1,1,n,m,'')
