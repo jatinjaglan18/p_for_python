5bf989e16ccc1514731a382a4655e3c13e07b75c
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-08-06 20:48:58 +0530

    Climb stairs Down

---
+++ 10_DP/3.py
@@ -0,0 +1,16 @@
+#Climb Stairs down using Tabulation
+n = int(input())
+arr = [int(input()) for i in range(n)]
+
+def countPaths(n,arr):
+    dp = [0 for i in range(n+1)]
+    dp[n] = 1
+
+    for i in range(n-1,-1,-1):
+        for j in range(1,arr[i]+1):
+            if i + j <= n:
+                dp[i] += dp[i+j]
+        
+    return dp[0]
+
+print(countPaths(n,arr))
