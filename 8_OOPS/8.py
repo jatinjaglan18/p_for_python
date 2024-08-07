67f84faa5eafaed6608b90283421015a9c0e2aac
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-08-07 10:33:21 +0530

    tilt of binary tree

---
+++ 8_OOPS/8.py
@@ -0,0 +1,78 @@
+#Binary Tree
+
+class Node:
+    def __init__(self, data = None , left = None, right = None):
+        self.data = data
+        self.left = left
+        self.right = right
+        
+arr = [50,25,12,None,None,37,30,None,None,None,75,62,None,70,None,None,87,None,None]
+def genrateTree(arr):
+    stack = []
+    root = Node(arr[0])
+    stack.append([root, 1])
+
+    idx = 0
+    while stack:
+        top = stack[-1]
+        if top[1] == 1:
+            idx += 1
+            if arr[idx] != None:
+                new_node = Node(arr[idx])
+                top[0].left = new_node
+                top[1] += 1
+                stack.append([new_node,1])
+
+            else:
+                top[1] += 1
+        elif top[1] == 2:
+            idx += 1
+            if arr[idx] != None:
+                new_node = Node(arr[idx])
+                top[0].right = new_node
+                top[1] += 1 
+                stack.append([new_node,1])
+            else:
+                top[1] += 1
+        else:
+            stack.pop()
+
+    return root
+
+root = genrateTree(arr)
+def display(node):
+    if node == None:
+        return 
+    
+    if node.left == None:
+        l = '.'
+    else:
+        l = str(node.left.data)
+    
+    if node.right == None:
+        r = '.'
+    else:
+        r = str(node.right.data)
+
+    print(l + ' <-',node.data, '-> ' + r)
+    display(node.left)
+    display(node.right)
+
+display(root)
+
+#tilt of a binary tree
+tilt = 0
+def tilt_of_tree(node):
+    global tilt
+    if node == None:
+        return 0
+
+    l = tilt_of_tree(node.left)
+    r = tilt_of_tree(node.right)
+    s = l + r + node.data
+    tilt += abs(l-r)
+    return s 
+
+print(tilt_of_tree(root))
+print(tilt)
+
