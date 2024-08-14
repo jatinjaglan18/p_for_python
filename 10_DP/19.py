d4e5b1c521ba941060116812821e07d2dd57cda1
jatinjaglan18 <jatin.jaglan18@gmail.com>
2024-08-14 16:45:32 +0530

    Tiling m * n floor with m * 1

--- 10_DP/19.py
+++ 10_DP/19.py
@@ -1,6 +1,6 @@
 #Tiling with 2 * 1 
 
-#tiles -> length = 2 width = 1
+#tiles -> length = 1 width = 2
 length = int(input('length of floor: '))
 #width of floor = 2
 
@@ -31,3 +31,26 @@ def tiling(l):
 
 
 print(tiling(length))
+
+
+#Tiling m * n floor with m * 1
+
+#tile -> length = 1
+w = int(input('Width tile / floor: '))
+l = int(input('Length of floor: '))
+
+def tiling1(w,l):
+    dp = [0 for i in range(l+1)]
+
+    for i in range(1,l+1):
+        if i < w:
+            dp[i] = 1
+        elif i == w:
+            dp[i] = 2
+
+        else:
+            dp[i] = dp[i-1] + dp[i-w]
+        
+    return dp[l]
+
+print(tiling1(w,l))
